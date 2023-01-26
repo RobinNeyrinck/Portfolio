566a2be1459c419cfcc3da1f89d6a8ed22e68125
Robin <robin.neyrinck@student.howest.be>
2023-01-26 22:54:17 +0100

    added contact form structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Title from './Title';
 
 function Contact() {
-	return <div>Contact</div>;
+	return (
+		<div>
+			<div>
+				<form action="#" method="POST">
+					<Title>Contact</Title>
+					<input type="text" name="name" placeholder="Name"></input>
+					<input type="text" name="email" placeholder="Email"></input>
+					<textarea name="message" placeholder="Message" rows="10"></textarea>
+					<button type="button">Work with me!</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
