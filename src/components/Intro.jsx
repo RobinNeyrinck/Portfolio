685e742c314bf2607f160826bf1cbb467462fc87
Robin <robin.neyrinck@student.howest.be>
2023-01-26 21:55:25 +0100

    made basic intro of profile

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Intro() {
-	return <div>Intro</div>;
+	return (
+		<div>
+			<h1>Robin Neyrinck</h1>
+			<p>Software Engineer & Web developer</p>
+			<p className="font-bold">A developer-in-training, who codes for fun.</p>
+		</div>
+	);
 }
 
 export default Intro;
