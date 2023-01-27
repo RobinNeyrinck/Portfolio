43646afb76c2081799709d8cb82fc71e5058732b
Robin <robin.neyrinck@student.howest.be>
2023-01-27 11:04:58 +0100

    fixed images for deployment

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 export default [
 	{
 		title: 'My own portfolio',
-		imgUrl: './assets/images/portfolio.png',
+		imgUrl: '/assets/images/portfolio.png',
 		stack: ['React', 'Tailwind', 'Vite'],
 		link: 'https://github.com/R0B1NR0B41N/Portfolio',
 	},
