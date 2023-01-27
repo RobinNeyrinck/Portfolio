9bd2543587455c16ae66d8ec08440b263d276f6e
Robin <robin.neyrinck@student.howest.be>
2023-01-27 11:11:09 +0100

    added production data and the beginning of coding in timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,10 +1,17 @@
 export default [
 	{
 		year: '2021',
-		title: "Started Bachlor's degree at Howest",
+		title: "Started Bachelor's degree at Howest",
 		duration: '3 years',
 		details:
 			"Not being really sure about what I wanted to do, I started studying 'Applied Computer Science' at Howest Brugge. I was determined that computers would be my future, but I couldn't tell what route I wanted to take. Whether it would be security or software engineering.",
+	},
+	{
+		year: '2015',
+		title: 'First line of code',
+		duration: 'the beginning',
+		details:
+			'The first line of code, I started writing code in 2015. I loved playing GTA V Roleplay on the FiveM platform. That is why i started developing my own server by using pre-made scripts and altering them to fit to my needs. Usually it was HTML, CSS and a small bit of JavaScript that needed to be changed but later on I also started writing LUA code.',
 	},
 	// TODO: add more timeline items
 ];
