8b16d1ae96715ac01860e87d27e74c65fb19f876
Robin <robin.neyrinck@student.howest.be>
2023-01-27 10:05:11 +0100

    added styling for intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 function Intro() {
 	return (
-		<div>
-			<h1>Robin Neyrinck</h1>
-			<p>Software Engineer & Web developer</p>
-			<p className="font-bold">A developer-in-training, who codes for fun.</p>
+		<div className="flex flex-col items-center justify-center pt-20 pb-6 text-center">
+			<h1 className="mb-1 text-4xl font-bold dark:text-white md:mb-3 md:text-7xl">
+				Robin Neyrinck
+			</h1>
+			<p className="mb-3 text-base font-medium md:text-xl">
+				Software Engineer & Web developer
+			</p>
+			<p className="mb-6 max-w-xl text-sm font-bold">
+				A developer-in-training, who codes for fun.
+			</p>
 		</div>
 	);
 }
