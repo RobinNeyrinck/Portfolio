b37405c7f68912d023a02bc2969c0ae0d1058ae2
robin-alundi <159771477+robin-alundi@users.noreply.github.com>
2024-03-03 10:13:59 +0100

    cleanup stackitem + rebrand footer

--- src/components/stack/StackItem.jsx
+++ src/components/stack/StackItem.jsx
@@ -5,7 +5,7 @@ function StackItem({ title, icon }) {
   return (
     <div className="relative group">
       <p className="absolute top-full left-1/2 transform -translate-x-1/2 bg-gray-800 text-white px-2 py-1 rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100 dark:bg-stone-300 dark:text-stone-700">{title}</p>
-      <FontAwesomeIcon icon={icon} className="text-3xl relative z-0" />
+      <FontAwesomeIcon icon={icon} className="text-3xl" />
     </div>
   );
 }
