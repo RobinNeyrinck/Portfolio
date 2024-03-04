ab41f31f308337764cdcf00bae8a5bd848f1ce3f
robin-alundi <159771477+robin-alundi@users.noreply.github.com>
2024-03-04 15:43:40 +0100

    extra blog items

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -18,4 +18,28 @@ export default [
     thumbnail: "assets/images/thumbnails/tech-meet.png",
     link: "/tech-meet&mix",
   },
+  {
+    title: "Hendrik Manhaeve - UX",
+    tags: ["Attended Events", "Brugge", "Howest"],
+    thumbnail: "assets/images/thumbnails/ux.png",
+    link: "/hendrik-manhaeve-ux",
+  },
+  {
+    title: "Thomas De Bruyne - Testing",
+    tags: ["Attended Events", "Brugge", "Howest"],
+    thumbnail: "assets/images/thumbnails/testing.png",
+    link: "/thomas-de-bruyne-testing",
+  },
+  {
+    title: "Simon Verdonck - Combell",
+    tags: ["Attended Events", "Brugge", "Howest"],
+    thumbnail: "assets/images/thumbnails/combell.png",
+    link: "/simon-verdonck-combell",
+  },
+  {
+    title: "Campus Cloud Cafe",
+    tags: ["Attended Events", "Kortrijk", "Noest"],
+    thumbnail: "assets/images/thumbnails/campus-cloud-cafe.png",
+    link: "/campus-cloud-cafe",
+  },
 ];
