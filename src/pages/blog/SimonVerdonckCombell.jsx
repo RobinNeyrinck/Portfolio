846fc6782a0c465ef045f72e4d07f35ba1e4ec8f
robin-alundi <159771477+robin-alundi@users.noreply.github.com>
2024-03-10 09:42:20 +0100

    combell info

--- src/pages/blog/SimonVerdonckCombell.jsx
+++ src/pages/blog/SimonVerdonckCombell.jsx
@@ -1,7 +1,30 @@
 function SimonVerdonckCombell() {
-    return (
-        <p>This is a blog</p>
-    )
+  return (
+    <div>
+      <h1>Simon Verdonck – Combell</h1>
+      <img
+        src="assets/images/blogs/combell/thumbnail.png"
+        alt="thumbnail of Simon Verdonck – Combell"
+      />
+      <p>
+        In a session organized by Alexander, Simon Verdonck shared insights
+        about Combell. However, the talk took a marketing-oriented approach
+        rather than delving into the intricacies of hosting. The presentation
+        offered a promotional overview of Combell, falling short of providing
+        the anticipated in-depth exploration of hosting and its workings. While
+        it didn’t meet expectations in terms of technical depth, attendees
+        gained a promotional understanding of Combell’s services. In summary,
+        the event provided a marketing-focused perspective on Combell, leaving
+        the technical aspects of hosting somewhat unexplored.
+      </p>
+      <p>Tags:</p>
+      <ul>
+        <li>Attended Event</li>
+        <li>Brugge</li>
+        <li>Howest</li>
+      </ul>
+    </div>
+  );
 }
 
 export default SimonVerdonckCombell;
