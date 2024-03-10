434579040825928daa9763f7477aa67b2d633c54
robin-alundi <159771477+robin-alundi@users.noreply.github.com>
2024-03-10 10:53:37 +0100

    added internship to timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,17 +1,24 @@
 export default [
-	{
-		year: '2021',
-		title: "Started Bachelor's degree at Howest",
-		duration: '3 years',
-		details:
-			"Not being really sure about what I wanted to do, I started studying 'Applied Computer Science' at Howest Brugge. I was determined that computers would be my future, but I couldn't tell what route I wanted to take. Whether it would be security or software engineering.",
-	},
-	{
-		year: '2015',
-		title: 'First line of code',
-		duration: 'the beginning',
-		details:
-			'The first line of code, I started writing code in 2015. I loved playing GTA V Roleplay on the FiveM platform. That is why i started developing my own server by using pre-made scripts and altering them to fit to my needs. Usually it was HTML, CSS and a small bit of JavaScript that needed to be changed but later on I also started writing LUA code.',
-	},
-	// TODO: add more timeline items
+  {
+    year: "2024",
+    title: "Internship @ Alundi BV",
+    duration: "3 months",
+    details:
+      "I started my internship @ Alundi, here it was my task to help upgrade the mobile Ionic app to the latest version by using Capacitor instead of Cordova plugins. I also had to help out on the .NET backend, here I had to write an email engine so that we could send invoices for our clients. In this internship I did my Bachlor's thesis about online signatures. It took a lot of research to find the juridical and technical aspects of online signatures. As my PoC I had to write a small mobile app that could sign a document and send it to the client. It was an assingment that was given to me by the company, but I really enjoyed researching the juridical and technical aspects of the online signatures.",
+  },
+  {
+    year: "2021",
+    title: "Started Bachelor's degree at Howest",
+    duration: "3 years",
+    details:
+      "Not being really sure about what I wanted to do, I started studying 'Applied Computer Science' at Howest Brugge. I was determined that computers would be my future, but I couldn't tell what route I wanted to take. Whether it would be security or software engineering.",
+  },
+  {
+    year: "2015",
+    title: "First line of code",
+    duration: "the beginning",
+    details:
+      "The first line of code, I started writing code in 2015. I loved playing GTA V Roleplay on the FiveM platform. That is why i started developing my own server by using pre-made scripts and altering them to fit to my needs. Usually it was HTML, CSS and a small bit of JavaScript that needed to be changed but later on I also started writing LUA code.",
+  },
+  // TODO: add more timeline items
 ];
