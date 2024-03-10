318f7f8847204a0c6824ee3bb420e040540b42c6
robin-alundi <159771477+robin-alundi@users.noreply.github.com>
2024-03-10 09:40:48 +0100

    testing info

--- src/pages/blog/ThomasDeBruyneTesting.jsx
+++ src/pages/blog/ThomasDeBruyneTesting.jsx
@@ -1,7 +1,34 @@
 function ThomasDeBruyneTesting() {
-    return (
-        <p>This is a blog</p>
-    )
+  return (
+    <div>
+      <h1>Thomas De Bruyne – Testing</h1>
+        <img
+            src="assets/images/blogs/testing/thomas.png"
+            alt="Image of the speaker Thomas De Bruyne" />
+      <p>
+        In a recent session organized by Alexander, Thomas De Bruyne shared
+        valuable insights on automation, focusing on simplicity and
+        practicality. Thomas emphasized the significance of automation without
+        delving into complex examples. Despite the absence of a practical
+        segment, his talk provided a foundational understanding of applying
+        automation in real-world scenarios. Thanks to Alexander’s event
+        organization, the session remained accessible, ensuring that attendees
+        could easily grasp the concepts discussed. Thomas’s insights served as a
+        starting point for individuals looking to explore and implement
+        automation in their projects. In conclusion, Thomas De Bruyne’s
+        discussion on automation, facilitated by Alexander, offered a simplified
+        perspective, laying the groundwork for practical application in the
+        absence of hands-on examples.
+      </p>
+
+      <p>Tags:</p>
+      <ul>
+        <li>Attended Event</li>
+        <li>Brugge</li>
+        <li>Howest</li>
+      </ul>
+    </div>
+  );
 }
 
 export default ThomasDeBruyneTesting;
