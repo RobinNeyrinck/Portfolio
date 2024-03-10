04766edf40119981bcdf841421c6864b5e804761
robin-alundi <159771477+robin-alundi@users.noreply.github.com>
2024-03-10 09:39:16 +0100

    ux info

--- src/pages/blog/HendrikManhaeveUX.jsx
+++ src/pages/blog/HendrikManhaeveUX.jsx
@@ -1,7 +1,33 @@
 function HendrikManhaeveUX() {
-    return (
-        <p>This is a blog</p>
-    )
+  return (
+    <div>
+      <h1>Hendrik Manhaeve – UX</h1>
+      <img
+        src="assets/images/blogs/ux/hendrik.png"
+        alt="Image of the speaker Hendrik Manhaeve"
+      />
+      <p>
+        Organized by Alexander, a recent event featuring UX expert Hendrik
+        Manhaeve delivered a concise exploration of proper UX design. Hendrik
+        provided a comprehensive overview of UX principles, emphasizing the
+        importance of intuitive design and user-centric approaches. He shared
+        real-world examples, highlighting the impact of thoughtful UX on user
+        satisfaction and engagement. Attendees gained practical insights into
+        the UX journey, from concept to the final product. Hendrik’s expertise
+        equipped them with valuable knowledge for enhancing their own projects.
+        In a nutshell, thanks to Alexander’s event and Hendrik’s expertise,
+        participants left with a deeper understanding of UX design, ready to
+        apply these insights to create more user-friendly digital experiences.
+      </p>
+
+      <p>Tags:</p>
+      <ul>
+        <li>Attended Event</li>
+        <li>Brugge</li>
+        <li>Howest</li>
+      </ul>
+    </div>
+  );
 }
 
 export default HendrikManhaeveUX;
