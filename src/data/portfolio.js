4740f5172d64d22dcfa46cc2ffd5254021bd9f09
Robin Neyrinck <96230714+RobinNeyrinck@users.noreply.github.com>
2024-03-17 10:27:56 +0100

    images + portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,31 +1,38 @@
 export default [
-  {
-    title: "My own portfolio",
-    imgUrl: "/assets/images/portfolio.png",
-    stack: ["React", "Tailwind", "Vite"],
-    link: "https://github.com/R0B1NR0B41N/Portfolio",
-    description: "This is my own Portfolio, written in React.",
-  },
-  {
-    title: "Beerpoly",
-    imgUrl: "/assets/images/monopoly.png",
-    stack: ["Java", "Javascript", "HTML", "CSS"],
-    link: "https://github.com/R0B1NR0B41N/Monopoly",
-    description: "My first school project, a monopoly variant.",
-  },
-  {
-    title: "A game library",
-    imgUrl: "/assets/images/game-library.png",
-    stack: ["Vue", "Laravel"],
-    link: "https://github.com/R0B1NR0B41N/GameLibrary",
-    description:
-      "A small game library project, written in Vue and Laravel, with multilingual support.",
-  },
-  {
-    title: "Magic the Gathering",
-    imgUrl: "/assets/images/magic.png",
-    stack: [".NET", "Blazor", "Javascript", "HTML", "CSS"],
-    link: "https://github.com/R0B1NR0B41N/MagicTheGathering",
-    description: "A Magic the Gathering deck builder, written in Blazor.",
-  },
+	{
+		title: 'My own portfolio',
+		imgUrl: '/assets/images/portfolio.png',
+		stack: ['React', 'Tailwind', 'Vite'],
+		link: 'https://github.com/RobinNeyrinck/Portfolio',
+		description: 'This is my own Portfolio, written in React.',
+	},
+	{
+		title: 'Beerpoly',
+		imgUrl: '/assets/images/monopoly.png',
+		stack: ['Java', 'Javascript', 'HTML', 'CSS'],
+		link: 'https://github.com/RobinNeyrinck/Monopoly',
+		description: 'My first school project, a monopoly variant.',
+	},
+	{
+		title: 'A game library',
+		imgUrl: '/assets/images/game-library.png',
+		stack: ['Vue', 'Laravel'],
+		link: 'https://github.com/RobinNeyrinck/GameLibrary',
+		description:
+			'A small game library project, written in Vue and Laravel, with multilingual support.',
+	},
+	{
+		title: 'Magic the Gathering',
+		imgUrl: '/assets/images/magic.png',
+		stack: ['.NET', 'Blazor', 'Javascript', 'HTML', 'CSS'],
+		link: 'https://github.com/RobinNeyrinck/MagicTheGathering',
+		description: 'A Magic the Gathering deck builder, written in Blazor.',
+	},
+	{
+		title: 'Mars Project',
+		imgUrl: '/assets/images/mars.png',
+		stack: ['Java', 'Javascript', 'HTML', 'CSS'],
+		link: 'https://github.com/RobinNeyrinck/MarsProject',
+		description: 'A school project, a health application for on Mars.',
+	},
 ];
